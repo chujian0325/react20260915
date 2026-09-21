d3ed1c0ac663dc3303258c3a708ad2cecd07dbf2
chujian0325 <653510854@qq.com>
2026-09-21 14:40:12 +0800

    axios配置代理

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,76 +1,33 @@
 import { Component } from "react";
-import Header from "./components/Header/index.jsx";
-import List from "./components/List/index.jsx";
-import Footer from "./components/Footer/index.jsx";
-import "./App.css";
+import axios from "axios";
 
 // 创建并暴露App组件
 export default class App extends Component {
-  // 状态写在哪里，操作状态的方法就写在哪里
-  state = {
-    todos: [
-      { id: "001", name: "吃饭", done: true },
-      { id: "002", name: "睡觉", done: true },
-      { id: "003", name: "学习", done: false },
-      { id: "004", name: "打游戏", done: true },
-    ],
+  getStudentData = () => {
+    axios.get("/api1/students").then(
+      (response) => {
+        console.log('学生数据', response.data);
+      },
+      (error) => {
+        console.log('error', error);
+      },
+    );
   };
-
-  // addTodo用于添加一个todo，接收的参数是todo对象
-  // 如果子组件想修改父组件的数据，则父组件传递一个回调函数给子组件
-  addTodo = (todoObj) => {
-    this.setState({ todos: [todoObj, ...this.state.todos] });
-  };
-
-  updateTodo = (id, done) => {
-    const todos = this.state.todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, done };
-      } else {
-        return todo;
-      }
-    });
-    this.setState({ todos });
-  };
-
-  deleteTodo = (id) => {
-    const todos = this.state.todos.filter((todo) => {
-      return todo.id !== id;
-    });
-    this.setState({ todos });
-  };
-
-  handleClearAllDone = () => {
-    const todos = this.state.todos.filter((todo) => {
-      return !todo.done;
-    });
-    this.setState({ todos });
-  };
-
-  handleCheckAll = (done) => {
-    const todos = this.state.todos.map((todo) => {
-      return { ...todo, done };
-    });
-    this.setState({ todos });
-  };
-
+  getCarData = () => {
+    axios.get("/api2/cars").then(
+      (response) => {
+        console.log('汽车数据', response.data);
+      },
+      (error) => {
+        console.log('error', error);
+      },
+    );
+  }
   render() {
-    const { todos } = this.state;
     return (
-      <div className="todo_container">
-        <div className="todo_warp">
-          <Header addTodo={this.addTodo} />
-          <List
-            todos={todos}
-            updateTodo={this.updateTodo}
-            deleteTodo={this.deleteTodo}
-          />
-          <Footer
-            todos={todos}
-            handleCheckAll={this.handleCheckAll}
-            handleClearAllDone={this.handleClearAllDone}
-          />
-        </div>
+      <div>
+        <button onClick={this.getStudentData}>点我获取学生数据</button>
+        <button onClick={this.getCarData}>点我获取汽车数据</button>
       </div>
     );
   }
--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,22 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api1": {
+        //遇到/api的请求，代理到http://localhost:8080
+        target: "http://localhost:8080", //请求转发地址
+        changeOrigin: true, //控制服务器收到的请求头中host字段的值
+        rewrite: (path) => path.replace(/^\/api1/, ""), //重写请求路径（必须）
+      },
+      "/api2": {
+        target: "http://localhost:5001",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api2/, ""),
+      },
+    },
+  },
+});
