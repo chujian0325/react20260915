3a798f19983fea7c884d90dfb709057f9118cc3c
chujian0325 <653510854@qq.com>
2026-09-22 15:56:06 +0800

    fetch发请求

--- my-app/src/components/Search/index.jsx
+++ my-app/src/components/Search/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import PubSub from "pubsub-js";
 export default class Search extends Component {
-  onSearch = () => {
+  onSearch = async () => {
     console.log('Search组件发布消息')
 
     const {
@@ -15,23 +15,64 @@ export default class Search extends Component {
       isFirst: false,
       isLoading: true,
     });
-    axios.get(`https://api.github.com/search/users?q=${keyWord}`).then(
-      (response) => {
-        console.log(response);
-        PubSub.publish('search', {
-          isFirst: false,
-          isLoading: false,
-          users: response.data.items,
-        });
-      },
-      (error) => {
-        PubSub.publish('search', {
-          isFirst: false,
-          isLoading: false,
-          err: error.message,
-        });
-      },
-    );
+    // axios.get(`https://api.github.com/search/users?q=${keyWord}`).then(
+    //   (response) => {
+    //     console.log(response);
+    //     PubSub.publish('search', {
+    //       isFirst: false,
+    //       isLoading: false,
+    //       users: response.data.items,
+    //     });
+    //   },
+    //   (error) => {
+    //     PubSub.publish('search', {
+    //       isFirst: false,
+    //       isLoading: false,
+    //       err: error.message,
+    //     });
+    //   },
+    // );
+
+    // 使用fetch发请求(未优化)
+    // fetch(`https://api.github.com/search/users?q=${keyWord}`).then(
+    //   (response) => {
+    //     console.log('联系服务器成功了',);
+    //     return response.json(); //返回一个Promise对象
+    //   },
+    // ).then(
+    //   (data) => {
+    //     console.log('解析数据成功了',data);
+    //     PubSub.publish('search', {
+    //       isFirst: false,
+    //       isLoading: false,
+    //       users: data.items,
+    //     });
+    //   },
+    // ).catch(err => {
+    //     console.log('catch',err);
+    //   }
+    // )
+
+
+    // 使用fetch发请求(优化)
+    try {
+      const resp = await fetch(`https://api.github.com/search/users?q=${keyWord}`)
+      console.log('resp', resp);
+      const data = await resp.json();
+      console.log('data', data)
+      PubSub.publish('search', {
+        isFirst: false,
+        isLoading: false,
+        users: data.items,
+      })
+    } catch (error) {
+      console.log('catch', error);
+      PubSub.publish('search', {
+        isFirst: false,
+        isLoading: false,
+        err: error.message,
+      })
+    }
   };
 
   render() {
